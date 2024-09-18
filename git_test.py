d2b57b0f1c061f76716dd128d0c1746376e20b47
ZhukovGK <zhukovgk@tutamail.com>
2024-09-18 10:06:37 +0300

    First commit ever

---
+++ git_test.py
@@ -0,0 +1,16 @@
+# Пробный файл для проверки работоспособности git
+import numpy
+
+
+print('iouyhikjhkj', input())
+
+
+
+
+'''print(input())
+
+def cat():
+    print('meow')
+
+def dog():
+    print('wof')'''
